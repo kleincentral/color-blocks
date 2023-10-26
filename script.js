228b479d3dd1186c16e565a5c85777b37cbb8d6d
Mark Klein <kleincentral@gmail.com>
2023-10-26 14:32:43 -0500

    Made a list of buttons, deleted the starter cubes, made cubes add a colored box when you clicked the button, made the count properly reflect which color box was added, fixed the delete function so when you click on a cube it properly deletes.

--- script.js
+++ script.js
@@ -1,1 +1,61 @@
 console.log('🟥 🟦 🟩 🟨');
+
+function buttonclick(id) {
+    // let clickCreate = document.getElementById(id);
+    let clickCreate = document.getElementById('blocks');
+    console.log(clickCreate)
+    if (id === "red-btn"){
+        clickCreate.innerHTML += `<p onclick="deleteClick(event, 'red')" class="block red-fill"></p>`
+        let update = document.getElementById('red-count')
+        let newText = Number(update.textContent);
+        newText += 1
+        update.textContent = newText
+    }
+    else if (id === "blue-btn") {
+        clickCreate.innerHTML += `<p onclick="deleteClick(event, 'blue')" class="block blue-fill"></p>`
+        let update = document.getElementById('blue-count')
+        let newText = Number(update.textContent);
+        newText += 1
+        update.textContent = newText
+    }
+    else if (id === "green-btn") {
+        clickCreate.innerHTML += `<p onclick="deleteClick(event, 'green')" class="block green-fill"></p>`
+        let update = document.getElementById('green-count')
+        let newText = Number(update.textContent);
+        newText += 1
+        update.textContent = newText    
+    }
+    else if (id === "yellow-btn") {
+        clickCreate.innerHTML += `<p onclick="deleteClick(event, 'yellow')" class="block yellow-fill"></p>`
+        let update = document.getElementById('yellow-count')
+        let newText = Number(update.textContent);
+        newText += 1
+        update.textContent = newText    
+    }
+}
+
+
+function deleteClick(event, color){
+    console.log('Deleting', event.target);
+    event.target.remove();
+    let update = '';
+    if (color === 'blue') {
+        update = document.getElementById('blue-count')
+    }
+    else if (color === 'red') {
+        update = document.getElementById('red-count')
+    }
+    else if (color === 'yellow') {
+        update = document.getElementById('yellow-count')
+    }
+    else if (color === 'green') {
+        update = document.getElementById('green-count')
+    }
+    else {
+        console.log("ERROR!")
+        return
+    }
+    let newText = Number(update.textContent);
+    newText -= 1
+    update.textContent = newText
+}
